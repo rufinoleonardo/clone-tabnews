2f25a44478c48152e41f86bcf57c8103df82a89a
Leonardo Rufino <rufinodevelopment@gmail.com>
2025-09-12 16:19:39 -0300

    Add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1 + 1 as sum;");
+  console.log(result.rows);
+
   response.status(200).json({ msg: "Eu sou um programador acima da média." });
 }
 
