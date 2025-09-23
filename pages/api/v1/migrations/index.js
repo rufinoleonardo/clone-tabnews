7b3fc05456e1bd6fe7e1e5aea03235cb50598760
Leonardo Rufino <rufinodevelopment@gmail.com>
2025-09-23 16:43:13 -0300

    fix the migrations endpoint bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,14 @@ import migrationsRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+
+  if (!allowedMethods.includes(request.method)) {
+    return response
+      .status(405)
+      .json({ error: `Method "${request.method}" is not allowed.` });
+  }
+
   const dbClient = await database.getNewClient();
 
   const defaultMigrationsProps = {
@@ -14,28 +22,29 @@ export default async function migrations(request, response) {
     migrationsTable: "pgmigrations",
   };
 
-  if (request.method == "GET") {
-    const pendingMigrations = await migrationsRunner(defaultMigrationsProps);
+  try {
+    if (request.method == "GET") {
+      const pendingMigrations = await migrationsRunner(defaultMigrationsProps);
 
-    await dbClient.end();
-
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method == "POST") {
-    const executedMigrations = await migrationsRunner({
-      ...defaultMigrationsProps,
-      dryRun: false,
-    });
-
-    await dbClient.end();
-
-    if (executedMigrations.length > 0) {
-      return response.status(201).json(executedMigrations);
+      return response.status(200).json(pendingMigrations);
     }
 
-    return response.status(200).json(executedMigrations);
-  }
+    if (request.method == "POST") {
+      const executedMigrations = await migrationsRunner({
+        ...defaultMigrationsProps,
+        dryRun: false,
+      });
 
-  return response.status(405).end();
+      if (executedMigrations.length > 0) {
+        return response.status(201).json(executedMigrations);
+      }
+
+      return response.status(200).json(executedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    dbClient.end();
+  }
 }
