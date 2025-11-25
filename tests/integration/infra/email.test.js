8e9b86f63186d6ea805de6e69e5ecc80cd33c3ba
Leonardo Rufino <rufinodevelopment@gmail.com>
2025-11-25 17:19:26 -0300

    feat: add and configure `email` local infrastructure to the project

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -1,12 +1,33 @@
 import email from "infra/email";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllProcesses();
+});
 
 describe("infra/email.js", () => {
   test("send()", async () => {
+    await orchestrator.deleteAllEmails();
+
     await email.send({
       from: "LeoGmail <leonardo@email.com>",
       to: "rufinoleonardo@outlook.com",
       subject: "Teste de assunto",
       text: "Teste de corpo",
     });
+
+    await email.send({
+      from: "LeoGmail <leonardo@email.com>",
+      to: "rufinoleonardo@outlook.com",
+      subject: "Segundo e-mail",
+      text: "Teste de corpo 2o email",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+
+    expect(lastEmail.sender).toBe("<leonardo@email.com>");
+    expect(lastEmail.recipients[0]).toBe("<rufinoleonardo@outlook.com>");
+    expect(lastEmail.subject).toBe("Segundo e-mail");
+    expect(lastEmail.text).toBe("Teste de corpo 2o email\n");
   });
 });
